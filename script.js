aa724f9e3503da5949bc011b468a0c3b55c91fde
Rachna <rachnakum0476@gmail.com>
2023-05-17 06:53:54 +0530

    second

--- script.js
+++ script.js
@@ -6,20 +6,22 @@ let nameInput = document.querySelector('#name');
 let costInput = document.querySelector('#cost');
 let  difficultyInput = document.querySelector('#difficulty');
 
+let timeInput = document.querySelector('#time');
 
 
 btnAdd.addEventListener('click', () => {
     let name = nameInput.value;
     let cost = costInput.value;
     let difficulty = difficultyInput.value;
-    
+    let time = timeInput.value;
+
 
     let template = `
                 <tr>
                     <td>${name}</td>
                     <td>${cost}</td>
                     <td>${difficulty}</td>
-                    <td>${difficulty}</td>
+                    <td>${time}</td>
                
                
                 </tr>`;
